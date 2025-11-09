c8c6c68a6846388e1301a3dac372460a41b8daea
Rob Trehy <robert.trehy@outlook.com>
2025-11-09 15:39:13 +0000

    Add new config option for exploding string

--- src/config/user-preferences.php
+++ src/config/user-preferences.php
@@ -13,5 +13,17 @@ return [
     'defaults' => [
         // 'Default Preferences go here
         // 'key' => 'value'
-    ]
+    ],
+    /**
+     * When preference is set, the provided string will be used
+     * as the key by default.
+     * 
+     * With this option enabled the key string will be exploded 
+     * by the character in the second option.
+     * 
+     * E.g "notification.email" = {value} would become 
+     * ["notification" => ["email" => {value}]]
+     */
+    'explode-key-strings' => false,
+    'explode-key-character' => '.',
 ];
